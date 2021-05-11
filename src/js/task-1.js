e3757b7376dac0807877cc32138045677e41097a
Julia Kutsyn <yuliyaflm@gmail.com>
2021-05-11 15:50:35 +0300

    updates task-1

--- src/js/task-1.js
+++ src/js/task-1.js
@@ -1,5 +1,10 @@
 const delay = ms => {
   // Твой код
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(ms);
+    }, ms);
+  });
 };
 
 const logger = time => console.log(`Resolved after ${time}ms`);
