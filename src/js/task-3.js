fdadb334e68065b74462026c5e30df78b1ae98e4
Julia Kutsyn <yuliyaflm@gmail.com>
2021-05-12 21:21:00 +0300

    fixed task-3

--- src/js/task-3.js
+++ src/js/task-3.js
@@ -18,7 +18,7 @@ const makeTransaction = transaction => {
   });
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ({ id, time }) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
